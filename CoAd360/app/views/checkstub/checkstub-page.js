782fdb5d7bc219cd16b4969e2d88ec5c09f0f9db
Chandra Prakash <chandra.prakash@infobeans.com>
2015-09-24 09:55:41 +0000

    Commit for checkstub. Changes made by Chandra Prakash (chandra.prakash@infobeans.com).

--- CoAd360/app/views/checkstub/checkstub-page.js
+++ CoAd360/app/views/checkstub/checkstub-page.js
@@ -1,11 +1,11 @@
 
 var frameModule = require("ui/frame");
 var viewModule = require("ui/core/view");
+var dependencyObservableModule = require("ui/core/dependency-observable");
 var viewModel;
+var page;
 
 function pageLoaded(args) {
-    
-    var page = args.object;
     page = args.object;
     
     var navigationBar = viewModule.getViewById(page, "navigationBar");
@@ -26,3 +26,33 @@ function backButtonPressed(args) {
     frameModule.topmost().goBack()
 }
 exports.backButtonPressed = backButtonPressed;
+
+function tapOnView(args) {
+    var searchBar = viewModule.getViewById(page, "search");
+    if (searchBar.android) {
+        searchBar.android.clearFocus();
+    }
+    if (searchBar.ios) {
+        searchBar.ios.resignFirstResponder();
+    }
+}
+exports.tapOnView = tapOnView;
+
+var PieDataModel = (function (_super) {
+    __extends(PieDataModel, _super);
+    function PieDataModel() {
+        _super.call(this);
+        this.initData();
+    }
+    PieDataModel.prototype.initData = function () {
+        this.set("pieSource", [
+            { Brand: "Audi", Amount: 10 },
+            { Brand: "Mercedes", Amount: 76 },
+            { Brand: "Fiat", Amount: 60 },
+            { Brand: "BMW", Amount: 24 },
+            { Brand: "Crysler", Amount: 40 }
+        ]);
+    };
+    return PieDataModel;
+})(dependencyObservableModule.DependencyObservable);
+exports.PieDataModel = PieDataModel;
