92dbec0d4326aa8f9f173d6f3aa3bf977793dd3d
Bipin Kumar <bipin.ahirwal@infobeans.com>
2015-09-25 17:37:27 +0000

    Updated for the dynamic layout of the fields Changes made by Bipin Kumar (bipin.ahirwal@infobeans.com).

--- CoAd360/app/views/mysummaryV2/mysummary-page.js
+++ CoAd360/app/views/mysummaryV2/mysummary-page.js
@@ -1,6 +1,9 @@
 var mySummaryViewModelModule = require("../../view-models/mysummary-view-model");
 var frameModule = require("ui/frame");
 var viewModule = require("ui/core/view");
+var gridLayout = require("ui/layouts/grid-layout");
+var labelModule = require("ui/label");
+var platformModule = require("platform");
 var viewModel;
 var page;
 
@@ -24,8 +27,59 @@ function pageLoaded(args) {
     	var heading = viewModule.getViewById(page, "heading");
 		heading.android.setGravity(17);
 	}
-    //var saveButton = viewModule.getViewById(page, "savebutton");
-    //saveButton.isEnabled=false;  
+    
+    var widthOfWidget = platformModule.screen.mainScreen.widthDIPs - 30;
+    var infoWidth = widthOfWidget/2 +10;
+    
+    var personalInformation = [{key: "Address",
+                                value: "123 Mulberry Street Apartment. 123"},
+                               {key: "City",
+                                value:"New York"},
+                               {key: "State",
+                                value:"NY"},
+                               {key: "Zip",
+                                value:"12345"},
+                               {key: "Home Phone",
+                                value:"123-456-7890"},
+                               {key: "Mobile Phone",
+                                value:"123-456-7890"},
+                               {key: "Personal Email",
+                                value:"lukehallem@gmail.com"},
+                               {key: "Work Email",
+                                value:"lukehallem@gmail.com"}
+                              ];
+    
+    var stackLayoutInformation = viewModule.getViewById(page, "stackLayoutForInformation");
+    
+    for (var i=0; i<personalInformation.length;i++)
+        {
+            
+            var gridLayoutInformation = new gridLayout.GridLayout();
+            gridLayoutInformation.row = "*";
+            stackLayoutInformation.addChild(gridLayoutInformation);
+            
+            
+            var keylabel = new labelModule.Label();
+            keylabel.text=personalInformation[i].key;
+            keylabel.width= infoWidth;
+            keylabel.cssClass= "leftLabel";
+            
+            gridLayoutInformation.addChild(keylabel);
+            
+            var valuelabel = new labelModule.Label();
+            valuelabel.text=personalInformation[i].value;
+            valuelabel.width= infoWidth;
+            valuelabel.cssClass= "rightLabel";
+            valuelabel.textWrap="true";
+            
+            gridLayoutInformation.addChild(valuelabel);
+            
+            var lineLabel = new labelModule.Label();
+            lineLabel.width= widthOfWidget;
+            lineLabel.cssClass="line";
+            
+            stackLayoutInformation.addChild(lineLabel);
+        }
 };
 exports.pageLoaded = pageLoaded;
 
