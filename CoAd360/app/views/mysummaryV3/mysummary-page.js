a7a86a29bdc81303a3243dfbd3f9dcebc6a2aa9c
Bipin Kumar <bipin.ahirwal@infobeans.com>
2015-09-28 16:41:37 +0000

    Added new version of my summary with separator line is only applied to section not the content Changes made by Bipin Kumar (bipin.ahirwal@infobeans.com).

---
+++ CoAd360/app/views/mysummaryV3/mysummary-page.js
@@ -0,0 +1,47 @@
+var mySummaryViewModelModule = require("../../view-models/mysummary-view-model");
+var frameModule = require("ui/frame");
+var viewModule = require("ui/core/view");
+var viewModel;
+var page;
+
+function pageLoaded(args) {
+    
+    page = args.object;
+    
+    //Set the binding context on the page.
+    viewModel = new mySummaryViewModelModule.MySummaryViewModel();
+    
+    //Set the binding context on the page.
+	page.bindingContext = viewModel;
+    
+    if (page.android)
+    {
+    	var heading = viewModule.getViewById(page, "heading");
+		heading.android.setGravity(17);
+	} 
+};
+exports.pageLoaded = pageLoaded;
+
+function backButtonPressed(args) {
+    frameModule.topmost().goBack()
+}
+exports.backButtonPressed = backButtonPressed;
+
+function tapOnView(args) {
+    var searchBar = viewModule.getViewById(page, "searchField");
+    if (searchBar.android) {
+        searchBar.android.clearFocus();
+    }
+    if (searchBar.ios) {
+        searchBar.ios.resignFirstResponder();
+    }
+}
+exports.tapOnView = tapOnView;
+
+function editButtonTap(args) {
+    frameModule.topmost().navigate({
+					moduleName: "./views/summaryedit/summaryedit-page",
+					animated: true
+          		});
+}
+exports.editButtonTap = editButtonTap;
