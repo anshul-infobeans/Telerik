ce431678b42d6afa72c40220c382c6b405783543
Bipin Kumar <bipin.ahirwal@infobeans.com>
2015-09-29 13:07:21 +0000

    Updated for api url and webservice method Changes made by Bipin Kumar (bipin.ahirwal@infobeans.com).

--- CoAd360/app/common/constants.js
+++ CoAd360/app/common/constants.js
@@ -1,7 +1,3 @@
 //login message
-exports.telerikApiKey = "Rw5X5HlnO1s9E0kf";
-exports.authenticationTokenKey = "authentication_token";
-exports.deleteProjectMessage = "Do you want to delete the project? This will delete all its tasks.";
-exports.deleteProjectHeader = "Delete project";
-exports.deleteTaskMessage = "Do you want to delete the task?";
-exports.deleteTaskHeader = "Delete task";
+exports.apiurl = "https://360apiqa.coadvantage.com";
+exports.loginWebservice="/Api/ValidateUser";
