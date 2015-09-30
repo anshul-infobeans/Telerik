adbc38ab9061a4a06d3e187203a520d702495aad
Bipin Kumar <bipin.ahirwal@infobeans.com>
2015-09-30 10:39:02 +0000

    Updated for my summary Changes made by Bipin Kumar (bipin.ahirwal@infobeans.com).

--- CoAd360/app/views/mysummaryV3/mysummary-page.js
+++ CoAd360/app/views/mysummaryV3/mysummary-page.js
@@ -1,6 +1,7 @@
 var mySummaryViewModelModule = require("../../view-models/mysummary-view-model");
 var frameModule = require("ui/frame");
 var viewModule = require("ui/core/view");
+var labelModule = require("ui/label");
 var viewModel;
 var page;
 
@@ -18,7 +19,28 @@ function pageLoaded(args) {
     {
     	var heading = viewModule.getViewById(page, "heading");
 		heading.android.setGravity(17);
-	} 
+        
+        var termsofuse = viewModule.getViewById(page, "termsofuse");
+		termsofuse.android.setGravity(17);
+        
+        var privacypolicy = viewModule.getViewById(page, "privacypolicy");
+		privacypolicy.android.setGravity(17);
+        
+        var contactus = viewModule.getViewById(page, "contactus");
+		contactus.android.setGravity(17);
+
+
+        var gap = viewModule.getViewById(page, "android-gap");
+		gap.style.visibility="visible";
+        gap.height=50;
+	}
+    else
+        {
+            var gap = viewModule.getViewById(page, "android-gap");
+			gap.style.visibility="collapsed";
+        }
+
+    hideSearchKeyboard();
 };
 exports.pageLoaded = pageLoaded;
 
@@ -28,15 +50,19 @@ function backButtonPressed(args) {
 exports.backButtonPressed = backButtonPressed;
 
 function tapOnView(args) {
+    hideSearchKeyboard();
+}
+exports.tapOnView = tapOnView;
+
+function hideSearchKeyboard() {
     var searchBar = viewModule.getViewById(page, "searchField");
     if (searchBar.android) {
         searchBar.android.clearFocus();
     }
-    if (searchBar.ios) {
+    else if (searchBar.ios) {
         searchBar.ios.resignFirstResponder();
     }
 }
-exports.tapOnView = tapOnView;
 
 function editButtonTap(args) {
     frameModule.topmost().navigate({
